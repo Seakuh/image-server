936be374582687da53818227cc0cf5d8e065094b
Seakuh <eteranimal@protonmail.com>
2025-01-08 14:13:49 +0700

    add get endpoint

--- src/image/image.controller.ts
+++ src/image/image.controller.ts
@@ -1,11 +1,15 @@
-import { Controller, Post, Body, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Controller, Post, Body, UploadedFile, UseInterceptors, Query, Get, Res, Param } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { v4 as uuidv4 } from 'uuid';
 import * as fs from 'fs';
 import * as path from 'path';
+import { ImageService } from './image.service';
+import { Response } from 'express';
 
 @Controller('images')
 export class ImageController {
+    constructor(private readonly imageService: ImageService) {}
+
   @Post('upload')
   @UseInterceptors(FileInterceptor('file'))
   async uploadFile(
@@ -33,5 +37,29 @@ export class ImageController {
     fs.writeFileSync(fullPath, file.buffer);
 
     return { url: `/images/${folder}/${fileName}` };
+  }
+  @Get(':filename')
+  async getTransformedImage(
+    @Param('filename') filename: string,
+    @Query('width') width: string,
+    @Query('height') height: string,
+    @Query('quality') quality: string,
+    @Res() res: Response,
+  ) {
+    const imageWidth = parseInt(width) || 300; // Standardwerte
+    const imageHeight = parseInt(height) || 300;
+    const imageQuality = parseInt(quality) || 75;
+
+    try {
+      const filePath = await this.imageService.transformImage(
+        filename,
+        imageWidth,
+        imageHeight,
+        imageQuality,
+      );
+      return res.sendFile(filePath);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
   }
 }
